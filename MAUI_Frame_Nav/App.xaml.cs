40a3b337c70f06632c28c7e29f0883652a94cabd
shaunstummer <7850615+shaunstummer@users.noreply.github.com>
2023-11-07 14:17:26 +1100

    Update App Window

--- MAUI_Frame_Nav/App.xaml.cs
+++ MAUI_Frame_Nav/App.xaml.cs
@@ -8,5 +8,18 @@
 
             MainPage = new NavigationPage(new MainPage());
         }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 450;
+            const int newHeight = 750;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
+        }
     }
 }
